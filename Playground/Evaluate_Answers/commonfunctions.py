0d1fc48f4c00ed8e28828c458969bd85f1365134
MrHackaMan <49288970+MrHackaMan@users.noreply.github.com>
2022-01-08 19:37:31 +0200

    added solved papers , integrated ID and perspective correction

--- Playground/Evaluate_Answers/commonfunctions.py
+++ Playground/Evaluate_Answers/commonfunctions.py
@@ -25,17 +25,28 @@ from scipy import fftpack
 import math
 
 from skimage.util import random_noise
-from skimage.filters import median
+from skimage.filters import median,sobel_h, sobel, sobel_v,roberts, prewitt
 from skimage.feature import canny
 from skimage.measure import label
 from skimage.color import label2rgb
 from sklearn import cluster
 from skimage.filters import threshold_otsu,apply_hysteresis_threshold
-from skimage.morphology import closing,dilation
+from skimage.morphology import closing,dilation,thin
+
+from mlp_train import detect_digit
 
 
-# Edges
-from skimage.filters import sobel_h, sobel, sobel_v,roberts, prewitt
+def loadImage(path):
+    image = io.imread(path)
+    
+    if len(image.shape) == 4:
+        image = rgba2rgb(image)
+    
+    if len(image.shape) > 2:
+        image = (rgb2gray(image)*255).astype('uint8')
+    else:
+        image = (image).astype('uint8')
+    return image
 
 # Show the figures / plots inside the notebook
 def show_images(images,titles=None):
@@ -54,49 +65,124 @@ def show_images(images,titles=None):
         a.set_title(title)
         n += 1
     fig.set_size_inches(np.array(fig.get_size_inches()) * n_ims)
-    plt.show() 
-
-
-def showHist(img):
-    # An "interface" to matplotlib.axes.Axes.hist() method
-    plt.figure()
-    imgHist = histogram(img, nbins=256)
-    
-    bar(imgHist[1].astype(np.uint8), imgHist[0], width=0.8, align='center')
+    plt.show()  
 
 
 def perspective_correction(image):
+    def findContours1():
+        gray = np.uint8(rgb2gray(image.copy())*255)
+        _,binary = cv2.threshold(gray,125,255,cv2.THRESH_BINARY)
+        cnts = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if len(cnts)==2 else cnts[1]
+        return cnts
 
-    # load the image, convert it to grayscale, blur it
-    # slightly, then find edges
-    gray = np.uint8(rgb2gray(image)*256)
-    ret,binary = cv2.threshold(gray,125,255,cv2.THRESH_BINARY)
+    def findContours2():
+        gray = np.uint8(rgb2gray(image.copy())*255)
+        edged = np.uint8(canny(gray,sigma=4))
+        cnts = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if len(cnts)==2 else cnts[1]
+        return cnts
+    
+    
+    getContours = [findContours2, findContours1]
+    for Contours in getContours:
+        cnts = Contours()
+        
+        if len(cnts) > 0:
+            cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+            for c in cnts:
+                peri = cv2.arcLength(c, True)
+                approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+                # if our approximated contour has four points,
+                # then we can assume we have found the paper
+                if len(approx) == 4:
+                    x,y = image.shape[:2]
+                    if peri > (x+y)*2/8: 
+                        #checking if contour length is bigger than the original image's length/8
+                        paper = four_point_transform(image, approx.reshape(4, 2))
+                        return paper, True
 
-    # find contours in the edge map, then initialize
-    # the contour that corresponds to the document
-    cnts = cv2.findContours(binary, cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)
+    return image, False
 
-    cnts = cnts[0] if len(cnts)==2 else cnts[1]
+def removeShadow(img):
+    rgb_planes = cv2.split(img)
 
-    docCnt = None
-    # ensure that at least one contour was found
-    if len(cnts) > 0:
-        # sort the contours according to their size in
-        # descending order
-        cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-        # loop over the sorted contours
-        for c in cnts:
-            # approximate the contour
-            peri = cv2.arcLength(c, True)
-            approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-            # if our approximated contour has four points,
-            # then we can assume we have found the paper
-            if len(approx) == 4:
-                x,y = image.shape[:2]
-                if peri > (x+y)*2/8: 
-                    #checking if contour length is bigger than the original image's length/8
-                    paper = four_point_transform(image, approx.reshape(4, 2))
-                    return paper, True
+    result_planes = []
+    result_norm_planes = []
+    for plane in rgb_planes:
+        dilated_img = cv2.dilate(plane, np.ones((7,7), np.uint8))
+        bg_img = cv2.medianBlur(dilated_img, 21)
+        diff_img = 255 - cv2.absdiff(plane, bg_img)
+        norm_img = cv2.normalize(diff_img,None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+        result_planes.append(diff_img)
+        result_norm_planes.append(norm_img)
 
-    return None, False
+    result = cv2.merge(result_planes)
+    result_norm = cv2.merge(result_norm_planes)
+    return result
+    
+def get_id(img, id_length=7, show_info=False):
+    
+    image,_ = perspective_correction(img.copy())
+    image = image[:image.shape[0]//4,image.shape[1]//2:]
+    image = removeShadow(image)
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.medianBlur(gray, 5)
+
+    _,binary = cv2.threshold(gray,235,255,cv2.THRESH_BINARY)
+    contours,_ = cv2.findContours(binary,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+        
+    if show_info: print("Number of contours:" + str(len(contours)))
+        
+    data = []
+    data_i = []
+    for i,c in enumerate(contours):
+        x,y,w,h = cv2.boundingRect(c)
+        #if w > image.shape[0]/8 or h > image.shape[1]/8: #if very large contour 
+        #    continue
+        if w/h<0.8 or w/h>1.2: # if rec is not a square
+            continue
+        if cv2.contourArea(c) < 300: # if the square is so small
+            continue
+            
+        cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 2)
+        data.append((y,y+h,x,x+w))
+
+    if show_info: print("Number of contours:" + str(len(data)))
+
+        
+    id_str = id_length*'0'
+    if len(data) >= id_length:
+        # To take the low 7 squares in the image, to avoid the logo's detecction
+        data.sort(key=lambda element: element[0], reverse=True)
+        data = data[:id_length]
+        data.sort(key=lambda element: element[2])
+        ###
+
+        for y1,y2,x1,x2 in data:
+            data_i.append(thin(np.invert(binary[y1+2:y2-2,x1+2:x2-2].copy()),10))
+
+        id_str = ''
+        for i in range(id_length):
+            p = detect_digit(data_i[i], plot=show_info)
+            id_str += str(p)
+        
+
+    if show_info: 
+        plt.gray()
+
+        plt.figure(figsize=(15, 15))
+        plt.imshow(gray)
+        plt.title('gray')
+        plt.figure(figsize=(15, 15))
+        plt.imshow(binary)
+        plt.title('binary')
+        plt.figure(figsize=(15, 15))
+        plt.imshow(image)
+        plt.title('image');
+
+        show_images(data_i)
+        
+    return id_str
+    
---
+++ Playground/Evaluate_Answers/mlp_train.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+
+import numpy as np
+import torch
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+
+from skimage.morphology import binary_dilation, binary_erosion
+from skimage.transform import resize
+
+from six.moves import urllib
+from torchvision import datasets
+import matplotlib.pyplot as plt
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        
+        hidden_1 = 512
+        hidden_2 = 32
+        
+        self.fc1 = nn.Linear(28 * 28, hidden_1)
+        self.fc2 = nn.Linear(hidden_1, hidden_2)
+        self.fc3 = nn.Linear(hidden_2, 10)
+        
+        self.dropout = nn.Dropout(0.2)
+
+    def forward(self, x):
+        # flatten image input
+        x = x.reshape(-1, 28 * 28)
+        
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        
+        x = self.fc3(x)
+        return x
+
+
+
+def load_ds():
+    
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    urllib.request.install_opener(opener)
+
+    ############################################################
+
+    num_workers = 0
+    # how many samples per batch to load
+    batch_size = 20
+
+
+    # convert data to torch.FloatTensor
+    transform = transforms.Compose([transforms.RandomRotation(degrees=25),
+                                    transforms.ToTensor()
+                                    ])
+
+    # choose the training and test datasets
+    train_data = datasets.MNIST(root='data', train=True,
+                                       download=True, transform=transform)
+    test_data = datasets.MNIST(root='data', train=False,
+                                      download=True, transform=transform)
+
+    # prepare data loaders
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,
+        num_workers=num_workers)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, 
+        num_workers=num_workers)
+        
+    return(train_loader,test_loader)
+    
+
+
+def train(n_epochs=30):
+
+    train_loader = load_ds()[0]
+
+    model = Net()
+    criterion = nn.CrossEntropyLoss()
+
+
+    # specify optimizer (stochastic gradient descent) and learning rate = 0.01
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+
+    # number of epochs to train the model
+    #n_epochs = 30  # suggest training between 20-50 epochs
+
+    model.train() # prep model for training
+
+    for epoch in range(n_epochs):
+        # monitor training loss
+        train_loss = 0.0
+        
+        ###################
+        # train the model #
+        ###################
+        for data, target in train_loader:
+            # clear the gradients of all optimized variables
+            optimizer.zero_grad()
+            # forward pass: compute predicted outputs by passing inputs to the model
+            output = model(data)
+            # calculate the loss
+            loss = criterion(output, target)
+            # backward pass: compute gradient of the loss with respect to model parameters
+            loss.backward()
+            # perform a single optimization step (parameter update)
+            optimizer.step()
+            # update running training loss
+            train_loss += loss.item()*data.size(0)
+            
+        # print training statistics 
+        # calculate average loss over an epoch
+        train_loss = train_loss/len(train_loader.dataset)
+
+        print('Epoch: {} \tTraining Loss: {:.6f}'.format(
+            epoch+1, 
+            train_loss
+            ))
+
+    torch.save(model.state_dict(), 'model_weights.pt')
+    return model
+
+
+
+def evaluate(model):
+
+    test_loader = load_ds()[1]
+    criterion = nn.CrossEntropyLoss()
+    batch_size = 20
+    # initialize lists to monitor test loss and accuracy
+    test_loss = 0.0
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+
+    model.eval() # prep model for *evaluation*
+
+    for data, target in test_loader:
+        # forward pass
+        output = model(data)
+        # calculate the loss
+        loss = criterion(output, target)
+        # update test loss 
+        test_loss += loss.item()*data.size(0)
+        # convert output probabilities to predicted class
+        _, pred = torch.max(output, 1)
+        # compare predictions to true label
+        correct = np.squeeze(pred.eq(target.data.view_as(pred)))
+        # calculate test accuracy for each object class
+        for i in range(batch_size):
+            label = target.data[i]
+            class_correct[label] += correct[i].item()
+            class_total[label] += 1
+
+    # calculate and print avg test loss
+    test_loss = test_loss/len(test_loader.dataset)
+    print('Test Loss: {:.6f}\n'.format(test_loss))
+
+    for i in range(10):
+        if class_total[i] > 0:
+            print('Test Accuracy of %5s: %2d%% (%2d/%2d)' % (
+                str(i), 100 * class_correct[i] / class_total[i],
+                np.sum(class_correct[i]), np.sum(class_total[i])))
+        else:
+            print('Test Accuracy of %5s: N/A (no training examples)' % (classes[i]))
+
+    print('\nTest Accuracy (Overall): %2d%% (%2d/%2d)' % (
+        100. * np.sum(class_correct) / np.sum(class_total),
+        np.sum(class_correct), np.sum(class_total)))
+
+
+
+def detect_digit(img, plot=False):
+       
+    # image preprocessing
+    #im = resize(img, (28,28))
+    im = img>0.6
+    im = resize(img, (28,28))
+    #im = np.invert(im)
+    im = binary_dilation(im, [[1,1],[1,1]])
+
+    im = resize(im,(112,112))
+    im = resize(im,(28,28))
+    
+    im = np.uint8(im*255)
+    
+   
+    
+    if plot:
+        plt.gray()
+        plt.figure()
+        plt.imshow(im)
+    
+    
+    # Model loading
+    model = Net()
+    model.load_state_dict(torch.load('model_weights.pt'))
+    model.eval()
+    
+    transform = transforms.Compose([transforms.ToPILImage(),
+                                    transforms.Grayscale(),
+                                    #transforms.Resize((28,28)),
+                                    transforms.ToTensor()
+                                    ])
+    im = transform(im)
+    
+    output = model(im)
+    _, preds = torch.max(output, 1)
+    return preds.detach().numpy()[0]
+    
+
+    
+
+if __name__ == "__main__":
+    model = train()
+    evaluate(model)
